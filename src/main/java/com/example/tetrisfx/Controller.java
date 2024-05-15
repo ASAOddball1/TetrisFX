2370e399552fb5d6ab040688850ae959b9eaf824
Robbie Gordon <oddballishere@gmail.com>
2024-05-15 10:00:08 -0700

    asdasdas

--- src/main/java/com/example/tetrisfx/Controller.java
+++ src/main/java/com/example/tetrisfx/Controller.java
@@ -1,12 +1,19 @@
 package com.example.tetrisfx;
 import javafx.scene.shape.*;
 public class Controller {
-//Getting numbers and MESH from Tetris class
+    //Getting numbers and MESH from Tetris class
     public static final int MOVE = Tetris.MOVE;
     public static final int SIZE = Tetris.SIZE;
     public static int XMAX = Tetris.XMAX;
     public static int YMAX = Tetris.YMAX;
-    public static int [][] MESH = Tetris.MESH;
+    public static int[][] MESH = Tetris.MESH;
+
+    //Moving the blocks
+
+    public static void MoveRight (Form form){
+        if (form.a.getX() + MOVE <= XMAX - SIZE && form.b.get
+    }
+ 
 
 
 }
